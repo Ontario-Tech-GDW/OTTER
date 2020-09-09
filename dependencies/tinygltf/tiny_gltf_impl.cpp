a6981541f8670e13f0a5438ba8d283599ff984d9
samanthastahlke <samanthastahlke@gmail.com>
2020-09-09 10:21:41 -0400

    Removed STBI implementation defines from TinyGLTF to prevent multiple definition errors when linking against both statically

--- dependencies/tinygltf/tiny_gltf_impl.cpp
+++ dependencies/tinygltf/tiny_gltf_impl.cpp
@@ -1,5 +1,14 @@
 #define TINYGLTF_IMPLEMENTATION
-#define STB_IMAGE_IMPLEMENTATION
-#define STB_IMAGE_WRITE_IMPLEMENTATION
+
+/*Edit by Samantha on 09-09-2020:
+Commenting out the definitions for STBI's implementation and write implementation.
+When linking against both STB Image and TinyGLTF as static libs, these will
+cause multiple definition linker errors (since STB Image's static lib already 
+defines its own implementation).
+
+//#define STB_IMAGE_IMPLEMENTATION
+//#define STB_IMAGE_WRITE_IMPLEMENTATION
+*/
+
 // #define TINYGLTF_NOEXCEPTION // optional. disable exception handling.
 #include "tiny_gltf.h"
