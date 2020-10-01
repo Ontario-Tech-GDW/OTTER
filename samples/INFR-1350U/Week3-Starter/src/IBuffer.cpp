13d2b3d100822e70f7fb2f6557d4742207671215
Christian Riggi <christian.riggi@ontariotechu.net>
2020-10-01 00:20:17 -0400

    Implemented iBuffer

--- samples/INFR-1350U/Week3-Starter/src/IBuffer.cpp
+++ samples/INFR-1350U/Week3-Starter/src/IBuffer.cpp
@@ -5,15 +5,24 @@ IBuffer::IBuffer(GLenum type, GLenum usage) :
 	_elementSize(0),
 	_handle(0)
 {
-	// TODO: implement
+	_type = type;  
+	_usage = usage;   
+	glCreateBuffers(1, &_handle);
 }
 
 IBuffer::~IBuffer() {
-	// TODO: implement
+	
+	if (_handle != 0) {
+		glDeleteBuffers(1, &_handle);
+		_handle = 0; 
+	}
 }
 
 void IBuffer::LoadData(const void* data, size_t elementSize, size_t elementCount) {
-	// TODO: implement
+	// Note, this is part of the bindless state access stuff added in 4.5
+	glNamedBufferData(_handle, elementSize * elementCount, data, _usage);
+	_elementCount = elementCount;
+	_elementSize = elementSize;
 }
 
 void IBuffer::Bind() {
